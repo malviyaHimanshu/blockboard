350f87b1d9067084a828286e1b17d3d24a76525e
Sasank Madati <sasankmadati@gmail.com>
2023-04-16 01:49:16 +0530

    added temp1

--- Pc_version/web3/temp1.py
+++ Pc_version/web3/temp1.py
@@ -1,35 +1,23 @@
 import pyperclip
-from PIL import ImageGrab, Image
-import win32clipboard
 import time
 import subprocess
-import image
 import os
-
+import image
 # Initialize the clipboard data variable to an empty string
 clipboard_data = pyperclip.paste()
 # Loop forever
 while True:
     # Check if the clipboard data has changed
     new_clipboard_data = pyperclip.paste()
+    if new_clipboard_data == "":
+        print("image found")
+        image.getImage()
+        print("ss",pyperclip.paste())
 
     # result = subprocess.run(["node", "main.js"], capture_output=True, text=True)
     # print("prev= ", clipboard_data, " present= ", new_clipboard_data, " result: ", result.stdout.split('\n')[0])
     # when prev!= present
     if new_clipboard_data != clipboard_data:
-        print("new_clipboard_data: ", new_clipboard_data)
-        if pyperclip.paste() == '':
-            img = ImageGrab.grabclipboard()
-            if isinstance(img, Image.Image) == True:
-                print("Clipboard contains an image! Calling getImage()...")
-                image.getImage()
-                clipboard_data = pyperclip.paste()
-                print("clipboard_data: ", clipboard_data)
-                print("done")
-                continue
-            else:
-                print("Clipboard does not contain anything")
-                exit(2)
         print("updating blockchain")
         clipboard_data = new_clipboard_data
         text1 = clipboard_data
